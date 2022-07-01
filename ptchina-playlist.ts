a8a0a21bfd1c823a571848e3b3e0930c271f321a
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2022-07-01 20:56:21 -0300

    added MIT license

--- ptchina-playlist.ts
+++ ptchina-playlist.ts
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 interface IFile{
 	filename:string
 	extension:string
@@ -19,6 +17,31 @@ interface IThread{
 	postId:number
 }
 
+/**
+https://github.com/ussaohelcim/ptchina-playlist
+
+MIT License
+
+Copyright (c) 2022 ussaohelcim
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 
 async function threadToPlaylist(){
 
@@ -91,7 +114,7 @@ async function threadToPlaylist(){
 		document.body.removeChild(a)
 	
 	}
-	
+
 	const thread = await getThread()
 	const files = await getMedia(thread)
 	const playlist = await createPlaylist(files)
@@ -104,5 +127,3 @@ async function threadToPlaylist(){
 		console.log('No video/audio files in this thread.')
 	}
 }
-
-threadToPlaylist()
